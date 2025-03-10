76395307630d90d135b3b540927ab316265af8ef
jjanzen <jjanzen@jjanzen.ca>
2025-03-10 15:21:05 -0500

    add license notice

--- pomodoro-mode.el
+++ pomodoro-mode.el
@@ -1,5 +1,21 @@
 ;; pomodoro-mode.el --- Introduces a pomodoro timer to your mode-line -*- lexical-binding: t -*-
+
 ;; Copyright (C) 2025 jjanzen
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 ;; Author: j. janzen <jjanzen@jjanzen.ca>
 ;; Version: 0.2
 ;; Package-Requires: ()
