73093bdb220b10870bf3a29aa304816fb130af05
jjanzen <jjanzen@jjanzen.ca>
2025-03-10 14:35:44 -0500

    format things better

--- pomodoro-mode.el
+++ pomodoro-mode.el
@@ -1,61 +1,120 @@
-;; -*- lexical-binding: t -*-
+;; pomodoro-mode.el --- Introduces a pomodoro timer to your mode-line -*- lexical-binding: t -*-
+;; Copyright (C) 2025 jjanzen
+;; Author: j. janzen <jjanzen@jjanzen.ca>
+;; Version: 0.1
+;; Package-Requires: ()
+;; Keywords: productivity, convenience
 
-(defvar pomodoro-start-time 0)
-(defvar pomodoro-state 'working)
-(defconst pomodoro-work-time (* 25 60))
-(defconst pomodoro-break-time (* 5 60))
-(defconst pomodoro-work-symbol "🍅")
-(defconst pomodoro-break-symbol "🥫")
+;;; Commentary:
+;; This package provides a minor mode to add a simple pomodoro timer to your mode-line.
+;; To activate it, run `M-x pomodoro-global-mode`.
+;; To turn it off, run `M-x pomodoro-global-mode` once again.
 
+;;;###autoload
 (defgroup pomodoro nil
   "Pomodoro timer"
   :group 'convenience)
 
-(defun pomodoro-get-status-string ()
-  (defvar pomodoro-curr-time)
-  (defvar pomodoro-symbol)
-  (defvar pomodoro-delta)
-  (defvar pomodoro-max-time)
-  (defvar pomodoro-minutes)
-  (defvar pomodoro-seconds)
+;;;###autoload
+(defcustom pomodoro-start-time 0
+  "When the current pomodoro break/work session has begun"
+  :type '(integer)
+  :group 'pomodoro)
 
-  (setq pomodoro-curr-time (floor (float-time)))
-  (setq pomodoro-delta (- pomodoro-curr-time pomodoro-start-time))
+;;;###autoload
+(defcustom pomodoro-state 'working
+  "The current pomodoro state"
+  :type '(working break)
+  :group 'pomodoro)
+
+;;;###autoload
+(defcustom pomodoro-work-time (* 25 60)
+  "The length of a pomodoro work session in seconds"
+  :type '(integer)
+  :group 'pomodoro)
+
+;;;###autoload
+(defcustom pomodoro-break-time (* 5 60)
+  "The length of a pomodoro break session in seconds"
+  :type '(integer)
+  :group 'pomodoro)
+
+;;;###autoload
+(defcustom pomodoro-work-symbol "🍅"
+  "The symbol to display while in a work session"
+  :type '(string)
+  :group 'pomodoro)
+
+;;;###autoload
+(defcustom pomodoro-break-symbol "🥫"
+  "The symbol to display while in a break session"
+  :type '(string)
+  :group 'pomodoro)
+
+;;;###autoload
+(defun pomodoro-toggle-state ()
+  "Restart the pomodoro timer and switch to the other state."
+  (interactive)
   (cond ((equal pomodoro-state 'working)
-         (setq pomodoro-symbol pomodoro-work-symbol)
-         (setq pomodoro-max-time pomodoro-work-time))
+         (setq pomodoro-state 'break))
         ((equal pomodoro-state 'break)
-         (setq pomodoro-symbol pomodoro-break-symbol)
-         (setq pomodoro-max-time pomodoro-break-time))
+         (setq pomodoro-state 'working))
         (t
-         (setq pomodoro-symbol "")
-         (setq pomodoro-max-time 0)))
-  (if (>= pomodoro-delta pomodoro-max-time)
-      (setq pomodoro-delta 0)
-    (setq pomodoro-delta (- pomodoro-max-time pomodoro-delta)))
-  (setq pomodoro-minutes (/ pomodoro-delta 60))
-  (setq pomodoro-seconds (% pomodoro-delta 60))
+         (user-error "Invalid pomodoro state")))
+  (setq pomodoro-start-time (floor (float-time))))
 
-  (if (= pomodoro-delta 0)
-      (progn
-        (cond ((equal pomodoro-state 'working)
-               (setq pomodoro-state 'break))
-              ((equal pomodoro-state 'break)
-               (setq pomodoro-state 'working))
-              (t (message "should not occur")))
-        (setq pomodoro-start-time (floor (float-time)))))
-  (format " %s (%02d:%02d)" pomodoro-symbol pomodoro-minutes pomodoro-seconds))
+(defun pomodoro-get-status-string ()
+  "Get the status string for the pomodoro timer. May update the state if necessary."
+  (let ((curr-time) ;; the current Unix time
+        (symbol)    ;; the symbol to be displayed
+        (delta)     ;; the difference in time
+        (max-time)  ;; the maximum time that can be represented in the current state
+        (minutes)   ;; the number of minutes to display
+        (seconds))  ;; the number of seconds to display
 
-(progn
-  (setq pomodoro-start-time (- (floor (float-time)) 100))
-  (pomodoro-get-status-string))
 
-(define-minor-mode pomodoro-mode
-  "run a pomodoro timer"
+    ;; check how far into the countdown we are
+    (setq curr-time (floor (float-time)))
+    (setq delta (- curr-time pomodoro-start-time))
+    (cond ((equal pomodoro-state 'working)
+           (setq symbol pomodoro-work-symbol)
+           (setq max-time pomodoro-work-time))
+          ((equal pomodoro-state 'break)
+           (setq symbol pomodoro-break-symbol)
+           (setq max-time pomodoro-break-time))
+          (t
+           (setq symbol "")
+           (setq max-time 0)))
+    (if (>= delta max-time)
+        (setq delta max-time)
+      (setq delta (- max-time delta)))
+
+    ;; Get the time in minutes and seconds
+    (setq minutes (/ delta 60))
+    (setq seconds (% delta 60))
+
+    ;; Changing state here kinda violates the single-responsibility principle,
+    ;; but this also prevents me from needing to work with timers which might
+    ;; just be worse that a function that does more than one thing.
+    (if (<= delta 0)
+        (progn
+          (setq delta 0)
+          (pomodoro-toggle-state)))
+
+    (format " %s (%02d:%02d)" symbol minutes seconds)))
+
+(define-minor-mode pomodoro-local-mode
+  "Run a pomodoro timer. Use pomodoro-mode instead of this."
   :init-value nil
+  :group 'pomodoro
   :lighter (:eval (pomodoro-get-status-string)))
 
-(define-globalized-minor-mode global-pomodoro-mode
-  pomodoro-mode pomodoro-mode
+;;;###autoload
+(define-globalized-minor-mode pomodoro-mode
+  pomodoro-local-mode pomodoro-local-mode
+  "Initialize pomodoro-mode in all buffers"
+  :group 'pomodoro
   (setq pomodoro-state 'working)
   (setq pomodoro-start-time (floor (float-time))))
+
+(provide 'pomodoro-mode)
